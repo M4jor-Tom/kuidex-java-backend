00f31bc759edae84e1d71180836d9552f490e977
Tom <tom-vautray@hotmail.fr>
2022-11-04 20:39:54 +0100

    Initial commit

---
+++ src/main/java/com/kuidex/javabackend/KuidexJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kuidex.javabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuidexJavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuidexJavaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuidex/javabackend/KuidexJavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuidex.javabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuidexJavaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
